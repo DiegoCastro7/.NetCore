3acfa961b9f3cc436aa9b6cb64002ceec8518ffb
DiegoCastro7 <137318162+DiegoCastro7@users.noreply.github.com>
2023-11-07 13:13:57 -0500

    feat: :sparkles: Dieguito

---
+++ practicas/dieguito/Persistence/Data/dieguitoContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using System.Reflection;
+
+namespace Persistence.Data;
+
+public partial class dieguitoContext : DbContext
+{
+    public dieguitoContext()
+    {
+    }
+
+    public dieguitoContext(DbContextOptions<dieguitoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<PersonType> PersonTypes { get; set; }
+
+    public virtual DbSet<State> States { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
